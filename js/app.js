0da6db861197448c09f6a1b3a7da067004800f50
Becca Kennedy <bkennedy@agrilyst.com>
2019-01-06 14:20:59 -0500

    carousel functions thanks to cam

--- js/app.js
+++ js/app.js
@@ -28,24 +28,24 @@ $(document).ready(function(){
 
 });
 
-$('.owl-carousel').owlCarousel({
-	center:true,
-	loop:true,
-    margin:10,
-    nav:true,
-	animateOut: 'fadeOut'
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+// 	center:true,
+// 	loop:true,
+//     margin:10,
+//     nav:true,
+// 	animateOut: 'fadeOut',
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
 
 hljs.configure({tabReplace: '  '});
 hljs.initHighlightingOnLoad();
